e609a162b4964a7eaed59e88ae8eb1823876fa6d
root <pdragy@gmail.com>
2013-10-20 09:07:12 -0700

    date formatting, html formatting

--- configserver/web/root.py
+++ configserver/web/root.py
@@ -8,11 +8,11 @@ __date__      = '31 May 2012'
 
 import platform
 import cherrypy
-import datetime
+from datetime import datetime, date, time
 from configserver.tools.common import  get_version, render_template, info, error, success, warning
 
 class RootServer:
 	@cherrypy.expose
 	def index(self, **kwargs):
 		sw_version = get_version()
-		return render_template("index.html", sw_version = sw_version, current_time = datetime.datetime.now(), os_info = ', '.join(platform.uname()[:4]))
+		return render_template("index.html", sw_version = sw_version, current_time = datetime.now().strftime("%A, %d. %B %Y %I:%M%p"), os_info = ', '.join(platform.uname()[:4]))
