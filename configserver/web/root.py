09faf5614f978ad849da078307b7f99d09ffbf75
root <pdragy@gmail.com>
2013-10-20 12:09:33 -0700

    save arch

--- configserver/web/root.py
+++ configserver/web/root.py
@@ -6,6 +6,7 @@ __author__    = 'Jovan Brakus <jovan@brakus.rs>'
 __contact__   = 'jovan@brakus.rs'
 __date__      = '31 May 2012'
 
+import subprocess
 import platform
 import cherrypy
 from datetime import datetime, date, time
@@ -15,4 +16,4 @@ class RootServer:
 	@cherrypy.expose
 	def index(self, **kwargs):
 		sw_version = get_version()
-		return render_template("index.html", sw_version = sw_version, current_time = datetime.now().strftime("%A, %d. %B %Y %I:%M%p"), os_info = ', '.join(platform.uname()[:4]))
+		return render_template("index.html", sw_version = sw_version, current_time = datetime.now().strftime("%A, %d. %B %Y %I:%M%p"), os_info = ', '.join(platform.uname()[:4]), notify_color= subprocess.check_output(["tail", "-n", "1", "indicator_light"]))
